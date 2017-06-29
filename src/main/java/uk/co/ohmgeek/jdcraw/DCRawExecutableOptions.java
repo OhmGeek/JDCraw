56205be1ea2c33a48d07ccc6ab628f67896e6613
Ryan Collins <ryan@ohmgeek.co.uk>
2017-06-29 13:05:31 +0100

    Created basic classes (these obviously will change quite a lot)

---
+++ src/main/java/uk/co/ohmgeek/jdcraw/DCRawExecutableOptions.java
@@ -0,0 +1,10 @@
+package uk.co.ohmgeek.jdcraw;
+
+/**
+ * The uk.co.ohmgeek.jdcraw.DCRawExecutableOptions Class manages the arguments for the DCRaw executable.
+ * The idea is we can set options (i.e. flags) by calling functions, and then run these on the executable.
+ *
+ * Created by ryan on 29/06/17.
+ */
+public class DCRawExecutableOptions {
+}
---
+++ src/main/java/uk/co/ohmgeek/jdcraw/DCRawManager.java
@@ -0,0 +1,9 @@
+package uk.co.ohmgeek.jdcraw;
+
+/**
+ * The uk.co.ohmgeek.jdcraw.DCRawManager Class manages DCRaw operations.
+ * It's the key handler used to interface with the library.
+ * Created by ryan on 29/06/17.
+ */
+public class DCRawManager {
+}
