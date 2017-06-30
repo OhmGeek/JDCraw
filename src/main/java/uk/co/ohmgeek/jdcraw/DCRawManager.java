45c79dad5e5dda52c68590661f772fe80d405694
Ryan Collins <ryan@ohmgeek.co.uk>
2017-06-30 10:32:29 +0100

    Tidy DCRawManager code,and remove unnecessary modules since the change in logic

--- src/main/java/uk/co/ohmgeek/jdcraw/DCRawManager.java
+++ src/main/java/uk/co/ohmgeek/jdcraw/DCRawManager.java
@@ -1,6 +1,7 @@
 package uk.co.ohmgeek.jdcraw;
 
 import org.apache.commons.io.FilenameUtils;
+import uk.co.ohmgeek.jdcraw.operations.SetFileOutputType;
 
 import java.io.File;
 import java.io.IOException;
@@ -12,37 +13,44 @@ import java.util.List;
  * Created by ryan on 29/06/17.
  */
 public class DCRawManager {
-    private DCRawExecutableOptions options;
     private File file;
     private List<RAWOperation> operationList;
     public DCRawManager(File fileToProcess) {
-        this.options = null;
         this.file = fileToProcess;
     }
 
     public String process() throws IOException {
         ProcessBuilder dcrawProcessBuilder = new ProcessBuilder();
 
-        if(options == null) {
-            // use default options
-            options = new DCRawExecutableOptions();
-            System.out.println("Using default options, because none specified.");
-        }
-
-        // set the entire process to start running
-        dcrawProcessBuilder.command(OptionStringBuilder.build(options.getRendererArgs(), this.file));
-
         // start running the render process
         Process dcrawProcess = dcrawProcessBuilder.start();
 
         // get the filename
         // todo use a function to output this, as it will be tiff with -T arg, otherwise it will be different.
+        return getDestination(); //return the path of the destination.
+    }
+
+    private String getDestination() {
+        // first, get the path without extension
         String outputFilename = FilenameUtils.removeExtension(this.file.getPath());
-        outputFilename = outputFilename.concat(".tiff"); //this is for a tiff file
+
+        boolean isTiff = false;
+        // look through operations, and look at property of SetFileOutputType instance (if there is one).
+        //todo optimise for multiple arguments that are the same.
+        for (RAWOperation op: operationList) {
+            isTiff = op instanceof SetFileOutputType && op.getArgumentList().contains("-T");
+        }
+
+        //choose the correct extension
+        if(isTiff) {
+            outputFilename = outputFilename.concat(".tiff");
+        } else {
+            outputFilename = outputFilename.concat(".ppm");
+        }
+
         // now get the result.
         return outputFilename;
     }
-
     /**
      * Add an operation to be made by DCRaw
      * @param op : an instance of RawOperation to be added to the operation list.
