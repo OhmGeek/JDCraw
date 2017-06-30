c7a12ce97a126f7b31a67577ceb5231cbfa7bbef
Ryan Collins <ryan@ohmgeek.co.uk>
2017-06-30 13:06:10 +0100

    Created the two basic WB preset operations (for now)

---
+++ src/main/java/uk/co/ohmgeek/jdcraw/operations/SetWBCustomOperation.java
@@ -0,0 +1,14 @@
+package uk.co.ohmgeek.jdcraw.operations;
+
+import uk.co.ohmgeek.jdcraw.RAWOperation;
+
+import java.util.List;
+
+/**
+ * Created by ryan on 30/06/17.
+ */
+public class SetWBCustomOperation implements RAWOperation {
+    public List<String> getArgumentList() {
+        return null;
+    }
+}
---
+++ src/main/java/uk/co/ohmgeek/jdcraw/operations/SetWBPresetOperation.java
@@ -0,0 +1,14 @@
+package uk.co.ohmgeek.jdcraw.operations;
+
+import uk.co.ohmgeek.jdcraw.RAWOperation;
+
+import java.util.List;
+
+/**
+ * Created by ryan on 30/06/17.
+ */
+public class SetWBPresetOperation implements RAWOperation {
+    public List<String> getArgumentList() {
+        return null;
+    }
+}
